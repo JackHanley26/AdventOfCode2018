8716247ba3291a4db73bda1e842e282d821850a0
jackhanley <jack.hanley@mycit.ie>
2018-12-10 22:11:41 +0000

    Day 3

--- day_3/main.py
+++ day_3/main.py
@@ -5,62 +5,40 @@ with open('data.txt') as fp:
     data = [line.rstrip("\n\r") for line in fp]
 grid_size = 1000
 
+overlaps = {}
+
 
 def part_1():
     print("Running part 1")
 
     def extraction(line):
-        result = re.match(r'#([0-9]+)\ @\ ([0-9]+),([0-9]+):\ ([0-9]+)x([0-9]+)', line)
-        return {'id': int(result.group(1)),
-                'x': int(result.group(2)),
-                'y': int(result.group(3)),
-                'w': int(result.group(4)),
-                'h': int(result.group(5))}
-
-    def write_claim(c, fab):
-        # writing top down
-        start_across = c['x']
-        start_down = c['y']
-        width = c['w']
-        height = c['h']
-
-        # writing down
-        for y in range(start_down, start_down + height):
-            # writing across
-            for x in range(start_across, start_across + width):
-                fab[y][x].append(c['id'])
-
-
-    # todo: fix this code
-
-    fabric = [[[] for x in range(grid_size)] for x in range(grid_size)]
+        r = re.match(r'#([0-9]+)\ @\ ([0-9]+),([0-9]+):\ ([0-9]+)x([0-9]+)', line)
+        return {'id': int(r.group(1)), 'x': int(r.group(2)), 'y': int(r.group(3)), 'w': int(r.group(4)),
+                'h': int(r.group(5))}
 
     claims = [extraction(line) for line in data]
 
-    for claim in claims:
-        write_claim(claim, fabric)
+    fabric = defaultdict(list)
+    global overlaps
 
-    print('Doing calc')
-    overused = 0
-    for line in fabric:
-        for x in line:
-            if len(x) >= 2:
-                overused = overused + 1
-    print(overused)
+    for c in claims:
+        overlaps[c['id']] = set()
+        for y in range(c['y'], c['y'] + c['h']):
+            for x in range(c['x'], c['x'] + c['w']):
 
-    return fabric
+                if fabric[(x, y)]:
+                    for number in fabric[(x, y)]:
+                        overlaps[number].add(c['id'])
+                        overlaps[c['id']].add(number)
+                fabric[(x, y)].append(c['id'])
+
+    print(len([b for b in fabric if len(fabric[b]) > 1]))
+
 
 def part_2():
     print("Running part 2")
-
-    fabric = part_1()
-    not_used = set()
-    for line in fabric:
-        for x in line:
-            if len(x) == 0:
-                not_used.add(x['id'])
-
-    print(not_used)
+    global overlaps
+    print([b for b in overlaps if len(overlaps[b]) == 0][0])
 
 
 if __name__ == '__main__':
